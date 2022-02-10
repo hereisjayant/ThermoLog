ce1006215d56078aa7a4914783493fab7aef8f8b
Your Name <new2282307660@gmail.com>
2022-02-10 10:50:31 -0800

    remove flask restful

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,13 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
+CORS(app)
 
-class Quotes(Resource):
-    def get(self):
-        return {
-            'William Shakespeare': {
-                'quote': ['Love all,trust a few,do wrong to none',
-                'Some are born great, some achieve greatness, and some greatness thrust upon them.']
-        },
-        'Linus': {
-            'quote': ['Talk is cheap. Show me the code.']
-            }
-        }
-
-api.add_resource(Quotes, '/')
+@app.route('/', methods=['GET'])
+def testing():
+    if request.method == 'GET':
+        return 'Hello'
 
 if __name__ == '__main__':
     app.run(debug=True)
