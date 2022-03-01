9235c431b4128e33263fcb13771a30cd52850135
Phoenix Liu <55338489+phoenixthefirebird@users.noreply.github.com>
2022-03-01 10:18:30 -0800

    Backend v1.2: store dummy apis working (video fetching not implemented) (#5)
    * change route to store
    
    * create endpoint
    
    * find store function
    
    * update endpoint
    
    * delete store
    
    * dummy store first draft
    
    * update readme
    
    * update readme

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,13 @@ from flask import Flask, request
 from flask_cors import CORS
 
 from app.users import users
+from app.stores import stores
 # import firebase_admin
 
 app = Flask(__name__)
 CORS(app)
 
-# constants
+############## user endpoints ################
 userList = users
 
 @app.route('/user/<userId>/deleteUser', methods=['DELETE'])
@@ -58,6 +59,49 @@ def create_user():
 def get_all_users():
     return userList
 
+############## store endpoints ################
+
+storeList = stores 
+
+# TODO: fetch livestream endpoint
+
+@app.route('/store/<storeId>/deleteStore', methods=['DELETE'])
+async def delete_store(storeId):
+    delattr(storeList, storeId)
+    return True
+
+@app.route('/store/<storeId>/update', methods=['PUT'])
+async def update_store(storeId):
+    new_store = request.get_json()
+    storeList[storeId] = {key: new_store.get(key, storeList[storeId][key]) for key in storeList[storeId]}
+    return storeList[storeId]
+    
+
+@app.route('/store/<storeId>/byId', methods=['GET'])
+def find_store(storeId):
+    return storeList[storeId]
+    
+
+@app.route('/store/create', methods=['POST'])
+def create_store():
+    data = request.form
+    storeList['this is the test store'] = dict( #TODO: id should be from database
+        capacity = data.capacity,
+        customerCount = data.customerCount,
+        isSafe = data.isSafe,
+        temperatures = data.temperatures,
+        email = data.email,
+        name = data.name,
+        liveStreamIds = data.liveStreamIds,
+    )
+    return True # TODO: return False for error when actually connected to firebase
+    #TODO: actually put this into a database and then store the generated id from there
+
+
+@app.route('/store/getAll', methods=['GET'])
+def get_all_stores():
+    return storeList
+
 @app.route('/', methods=['GET'])
 def testing():
     if request.method == 'GET':
@@ -68,8 +112,6 @@ if __name__ == '__main__':
 
 
 
-# Todo
-# 1. finish the basic info endpoints and queries
-# 3. deploying on heroku
+# TODO
 # 2. integrate with the video api
 # 4. change the static data to connect to firebase
