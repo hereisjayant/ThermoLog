382b9c58cc34bd97338977f9802bd24e6b178440
Parsa Riahi <priahi12@gmail.com>
2022-03-04 15:18:50 -0800

    basic crud for user and store done

--- backend/app/main.py
+++ backend/app/main.py
@@ -78,7 +78,7 @@ def create_user():
         time = (datetime.now() - epoch).total_seconds() * 1000.0
 
         data["lastTime"] = data["safeTime"] = time
-        userList['userId1'] = data
+        # userList['userId1'] = data
         try:
             userDb.insert_one(data)
             return ('user created', 200)
@@ -94,9 +94,6 @@ def create_user():
         #     notificationToken=data["notificationToken"],
         #     storeIds=data["storeIds"] if data["storeIds"] is not None else [],
         # )
-        # TODO: return False for error when actually connected to MongoDB
-        return ('user created', 200)
-    # TODO: actually put this into a database and then store the generated id from there
 
 @app.route('/user/getAll', methods=['GET'])
 def get_all_users():
@@ -104,7 +101,7 @@ def get_all_users():
         userList = userDb.find({})
         return (userList, 200)
     except Exception as e:
-        return ("There was an error pulling all the user" + e, 417)
+        return ("There was an error pulling all the users" + e, 417)
 
 ############## store endpoints ################
 
@@ -113,85 +110,71 @@ def get_all_users():
 
 @app.route('/store/<storeId>/deleteStore', methods=['DELETE'])
 async def delete_store(storeId):
-    delattr(storeList, storeId)
-    return ("store deleted", 200)
+    try:
+        result = storeDb.delete_one({"_id": storeId})
+        return (result, 200)
+    except Exception as e:
+        return ("There was an error deleting the store" + e, 417)
 
 
 @app.route('/store/<storeId>/update', methods=['PUT'])
 async def update_store(storeId):
-    new_store = request.get_json()
-    storeList[storeId] = {key: new_store.get(
-        key, storeList[storeId][key]) for key in storeList[storeId]}
-    return (storeList[storeId], 200)
+    try:
+        new_store = request.get_json()
+        result = storeDb.replace_one({"_id": storeId}, new_store)
+        if result:
+            return (new_store, 200)
+        return ("there was an error updating the store", 417)
+    except Exception as e:
+        return ("There was an error updating the store" + e, 417)
 
 
 @app.route('/store/<storeId>/byId', methods=['GET'])
 def find_store(storeId):
-    return (storeList[storeId], 200)
+    try:
+        if storeId:
+            store = storeDb.find_one({"_id": storeId})
+            if store:
+                return (store, 200)
+            return ('store not found', 400)
+        else:
+            return ("invalid query", 400)
+    except Exception as e:
+        return ("There was an error querying the store" + e, 417)
 
 
 @app.route('/store/create', methods=['GET', 'POST'])
 def create_store():
-    data = request.form.to_dict()
-    data["isSafe"] = True
-    storeList['this is the test store'] = data
-    # dict(  # TODO: id should be from database
-    #     capacity=data["capacity"],
-    #     customerCount=data["customerCount"],
-    #     isSafe=data["isSafe"],
-    #     temperatures=data["temperatures"],
-    #     name=data["name"],
-    #     liveStreamIds=data["liveStreamIds"],
-    # )
-    # TODO: return False for error when actually connected to MongoDB
-    return ('created', 200)
-    # TODO: actually put this into a database and then store the generated id from there
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        data["isSafe"] = True
+        # storeList['this is the test store'] = data
+        try:
+            storeDb.insert_one(data)
+            return ('store created', 200)
+        except Exception as e:
+            return ("There was an error creating the store" + e, 417)
+        # dict(  # TODO: id should be from database
+        #     capacity=data["capacity"],
+        #     customerCount=data["customerCount"],
+        #     isSafe=data["isSafe"],
+        #     temperatures=data["temperatures"],
+        #     name=data["name"],
+        #     liveStreamIds=data["liveStreamIds"],
+        # )
 
 
 @app.route('/store/getAll', methods=['GET'])
 def get_all_stores():
-    return (storeList, 200)
+    try:
+        storeList = storeDb.find({})
+        return (storeList, 200)
+    except Exception as e:
+        return ("There was an error pulling all the stores" + e, 417)
 
 ############## store endpoints ################
 
 # TODO: fetch livestream endpoint
-
-@app.route('/store/<storeId>/deleteStore', methods=['DELETE'])
-async def delete_store(storeId):
-    delattr(storeList, storeId)
-    return True
-
-@app.route('/store/<storeId>/update', methods=['PUT'])
-async def update_store(storeId):
-    new_store = request.get_json()
-    storeList[storeId] = {key: new_store.get(key, storeList[storeId][key]) for key in storeList[storeId]}
-    return storeList[storeId]
-    
-
-@app.route('/store/<storeId>/byId', methods=['GET'])
-def find_store(storeId):
-    return storeList[storeId]
-    
-
-@app.route('/store/create', methods=['POST'])
-def create_store():
-    data = request.form
-    storeList['this is the test store'] = dict( #TODO: id should be from database
-        capacity = data.capacity,
-        customerCount = data.customerCount,
-        isSafe = data.isSafe,
-        temperatures = data.temperatures,
-        email = data.email,
-        name = data.name,
-        liveStreamIds = data.liveStreamIds,
-    )
-    return True # TODO: return False for error when actually connected to MongoDB
-    #TODO: actually put this into a database and then store the generated id from there
-
-
-@app.route('/store/getAll', methods=['GET'])
-def get_all_stores():
-    return storeList
 
 @app.route('/', methods=['GET'])
 def testing():
