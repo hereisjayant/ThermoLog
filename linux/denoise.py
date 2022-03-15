f849ff5d8a124a9f1ebb706bb38df0d80d3b0cd7
Ethan Shen <ethan.shen1031@gmail.com>
2022-03-15 09:54:00 -0700

    python denoiser working

--- linux/denoise.py
+++ linux/denoise.py
@@ -1,10 +1,25 @@
-from ctypes import *
+import ctypes
 from PIL import Image
 import numpy
 
 # open hardware accelerator shared object
 so_file = "/home/root/Desktop/accelerator.so"
-accelerator = CDLL(so_file)
+accelerator = ctypes.cdll.LoadLibrary(so_file)
+
+# define accelerator function
+compute_pixel = accelerator.compute_pixel
+compute_pixel.restype = ctypes.c_int
+compute_pixel.argtypes = [
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int
+]
 
 # initialize hardware accelerator memory mapping
 accelerator.init_hw();
@@ -21,20 +36,20 @@ arr = numpy.array(img.getdata())
 # saved processed image to 3d array
 result = numpy.zeros((height, width, depth), dtype=numpy.uint8)
 
-# for i in range(1, height - 1):
-#     for j in range(1, width - 1):
-#         for d in range(depth):
-#             result[i][j][d] = accelerator.compute_pixel(
-#                                         arr[(i - 1) * width + j - 1][d],
-#                                         arr[(i - 1) * width + j][d],
-#                                         arr[(i - 1) * width + j + 1][d],
-#                                         arr[i * width + j - 1][d],
-#                                         arr[i * width + j][d],
-#                                         arr[i * width + j + 1][d],
-#                                         arr[(i + 1) * width + j - 1][d],
-#                                         arr[(i + 1) * width + j][d],
-#                                         arr[(i + 1) * width + j + 1][d]
-#                                     )
+for i in range(1, height - 1):
+    for j in range(1, width - 1):
+        for d in range(depth):
+            result[i][j][d] = compute_pixel(
+                    arr[(i - 1) * width + j - 1][d],
+                    arr[(i - 1) * width + j][d],
+                    arr[(i - 1) * width + j + 1][d],
+                    arr[i * width + j - 1][d],
+                    arr[i * width + j][d],
+                    arr[i * width + j + 1][d],
+                    arr[(i + 1) * width + j - 1][d],
+                    arr[(i + 1) * width + j][d],
+                    arr[(i + 1) * width + j + 1][d]
+                )
 
 # write processed image to png
 result_img = Image.fromarray(result, "RGB")
