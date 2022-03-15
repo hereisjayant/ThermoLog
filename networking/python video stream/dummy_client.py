00c37fa05118d276044bc46223ab4ca7279fcd2f
Your Name <new2282307660@gmail.com>
2022-03-15 10:54:17 -0700

    dummy client

--- networking/python video stream/dummy_client.py
+++ networking/python video stream/dummy_client.py
@@ -1,7 +1,9 @@
-import io
+# import io
 import socket
 import struct
-import time
+# import time
+
+image = "swallow.jpg"
 
 client_socket = socket.socket()
 
@@ -11,26 +13,17 @@ client_socket.connect(('206.87.235.152', 8000))  # ADD IP HERE
 connection = client_socket.makefile('wb')
 try:
 
-    # Note the start time and construct a stream to hold image data
-    # temporarily (we could write it directly to connection but in thisa
-    # case we want to find out the size of each capture first to keep
-    # our protocol simple)
-    start = time.time()
-    stream = io.BytesIO()
-    for foo in camera.capture_continuous(stream, 'jpeg'):
-        # Write the length of the capture to the stream and flush to
-        # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
-        # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
-        # If we've been capturing for more than 30 seconds, quit
-        if time.time() - start > 60:
-            break
-        # Reset the stream for the next capture
-        stream.seek(0)
-        stream.truncate()
+    # open local image 
+    myfile = open(image, 'rb')
+    bytes = myfile.read()
+    size = len(bytes)
+
+
+    # Write the length of the capture to the stream and flush to
+    # ensure it actually gets sent
+    connection.write(struct.pack('<L', size))
+    connection.flush()
+    connection.write(bytes)
     # Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', 0))
 finally:
