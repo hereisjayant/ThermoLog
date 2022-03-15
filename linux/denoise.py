42239954cb58b7ec1e2059119ff9ef2efeb10387
Ethan Shen <ethan.shen1031@gmail.com>
2022-03-15 13:05:43 -0700

    created docs directory

--- linux/denoise.py
+++ linux/denoise.py
@@ -1,9 +1,13 @@
 import ctypes
 from PIL import Image
 import numpy
+import time
+
+# time program run time
+start_time = time.time()
 
 # open hardware accelerator shared object
-so_file = "/home/root/Desktop/accelerator.so"
+so_file = "./accelerator.so"
 accelerator = ctypes.cdll.LoadLibrary(so_file)
 
 # define accelerator function
@@ -37,25 +41,32 @@ arr = numpy.array(img.getdata())
 result = numpy.zeros((height, width, depth), dtype=numpy.uint8)
 
 for i in range(1, height - 1):
+    curr_row = i * width
+    prev_row = curr_row - width
+    next_row = curr_row + width
     for j in range(1, width - 1):
+        curr_col = j
+        prev_col = j - 1
+        next_col = j + 1
         for d in range(depth):
             result[i][j][d] = compute_pixel(
-                    arr[(i - 1) * width + j - 1][d],
-                    arr[(i - 1) * width + j][d],
-                    arr[(i - 1) * width + j + 1][d],
-                    arr[i * width + j - 1][d],
-                    arr[i * width + j][d],
-                    arr[i * width + j + 1][d],
-                    arr[(i + 1) * width + j - 1][d],
-                    arr[(i + 1) * width + j][d],
-                    arr[(i + 1) * width + j + 1][d]
+                    arr[prev_row + prev_col][d],
+                    arr[prev_row + curr_col][d],
+                    arr[prev_row + next_col][d],
+                    arr[curr_row + prev_col][d],
+                    arr[curr_row + curr_col][d],
+                    arr[curr_row + next_col][d],
+                    arr[next_row + prev_col][d],
+                    arr[next_row + curr_col][d],
+                    arr[next_row + next_col][d]
                 )
 
 # write processed image to png
 result_img = Image.fromarray(result, "RGB")
 result_img.save("result.png")
 
-print("Done")
+# print timing
+print("--- Done in %s seconds ---" % (time.time() - start_time))
 
 # release hardware accelerator memory mapping
 accelerator.close_hw();
