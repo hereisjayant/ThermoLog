0e27c72486af2da5b265b3bd62965385812f867b
Ethan Shen <ethan.shen1031@gmail.com>
2022-03-21 00:59:32 -0700

    client script working on de1

--- de1-software/client.py
+++ de1-software/client.py
@@ -1,25 +1,110 @@
-import socket
+import ctypes
+from numpy.ctypeslib import ndpointer
+from PIL import Image
+import numpy
+from socket import *
 import pickle
-import time
 
+height = 8
+width = 8
 
-s = socket.socket()
-s.connect((socket.gethostname(), 1234))
+# open hardware accelerator shared object
+so_file = "./accelerator.so"
+accelerator = ctypes.cdll.LoadLibrary(so_file)
 
+# define accelerator function
+compute_pixel = accelerator.compute_pixel
+compute_pixel.restype = ctypes.c_int
+compute_pixel.argtypes = [
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int
+]
 
-while True:
-    # this is the size of our buffer
-    msg = s.recv(1024)
-    arr = pickle.loads(msg)
-    print(arr)
+def denoise(arr):
 
-    # sleeps for 2.5 secs
-    time.sleep(2.5)
+    # create array to store result
+    result = numpy.zeros((height, width), dtype=numpy.uint32)
 
-    # processing the data (negating the array)
-    negative_arr = [[-val for val in row] for row in arr]
-    # pickling the data
-    pickled_negative_arr = pickle.dumps(negative_arr)
-    # sending it back to the server
-    s.send(pickled_negative_arr)
+    # initialize hardware accelerator
+    accelerator.init_hw();
 
+    # scan through image to apply the accelerator
+    for i in range(height):
+        for j in range(width):
+
+            curr_row = i
+            if i - 1 < 0: prev_row = i
+            else: prev_row = i - 1
+            if i + 1 >= height: next_row = i
+            else: next_row = i + 1
+
+            curr_col = j
+            if j - 1 < 0: prev_col = j
+            else: prev_col = j - 1
+            if j + 1 >= height: next_col = j
+            else: next_col = j + 1
+
+            result[i][j] = compute_pixel(
+                arr[prev_row][prev_col],
+                arr[prev_row][curr_col],
+                arr[prev_row][next_col],
+                arr[curr_row][prev_col],
+                arr[curr_row][curr_col],
+                arr[curr_row][next_col],
+                arr[next_row][prev_col],
+                arr[next_row][curr_col],
+                arr[next_row][next_col]
+            )
+
+    # release hardware accelerator
+    accelerator.close_hw();
+
+    return result
+
+def main():
+
+    # define server port number
+    serverName = gethostbyname("Ethans-MacBook-Pro.local")
+    serverPort = 12000
+
+    # creates client TCP socket
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+
+    # establish TCP connection with server
+    clientSocket.connect((serverName,serverPort))
+
+    print("Client socket connected to server at " + serverName)
+
+    while True:
+
+        # recieve bytes from server 
+        msg = clientSocket.recv(1024)
+
+        print("Received data from server")
+
+        # decode data
+        arr = numpy.frombuffer(msg, dtype=numpy.uint32).reshape((height, width))
+
+        print("Decoded data from server")
+
+        # process data
+        result = denoise(arr)
+
+        print("Processed data with hardware accelerator")
+
+        # send data to server
+        clientSocket.send(result.tostring())
+
+        print("Result sent to server succesfully")
+
+    clientSocket.close()
+
+if __name__ == "__main__":
+    main()
--- rpi-software/thermal_server.py
+++ rpi-software/thermal_server.py
@@ -1,35 +1,64 @@
-import socket
-import pickle
+from socket import *
+import time
+import numpy
+
+height = 8
+width = 8
 
 # Just a test function for creating 2d arr
-def create_2d_array(i):
-    rows, cols = (8, 8)
-    arr = [[i]*cols]*rows
-    return arr
+def create_2d_array():
 
-# server socket
-s = socket.socket()
-s.bind((socket.gethostname(), 1234))
-# max num of client that can connect
-s.listen(5)
+    values = [
+        50, 0, 50, 50, 0, 50, 50, 50, 
+        50, 50, 0, 50, 50, 0, 50, 50, 
+        50, 50, 50, 0, 50, 50, 0, 50, 
+        50, 0, 50, 50, 0, 50, 50, 50, 
+        50, 50, 0, 50, 50, 0, 50, 50, 
+        50, 50, 50, 0, 50, 50, 0, 50, 
+        50, 0, 50, 50, 0, 50, 50, 50, 
+        50, 50, 0, 50, 50, 0, 50, 50
+    ]
+
+    return numpy.array(values, dtype=numpy.uint32).reshape((height, width))
+
+# define server port number
+serverName = "0.0.0.0"
+serverPort = 12000
+
+# create TCP socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# associate the server port number with this socket
+serverSocket.bind((serverName, serverPort))
+
+# listen for TCP connections requests
+serverSocket.listen(1)
+
+# print host name
+print("The server is ready to receive at: " + gethostname())
+
+# accept incoming connection
+clientSocket, addr = serverSocket.accept()
+
+print("Connection from: " + str(addr))
 
 while True:
-    clientsocket, addr = s.accept()
-    print(f"Connection from {addr} has been established!")
-    i = 0 # just a counter for testing
-    # this will be an inf loop in our case
-    while i < 1000:
-        i+=1
-        arr = create_2d_array(i)
-        # this is how you convert python obj to pickle obj. to send over the socket
-        pickled_arr = pickle.dumps(arr) # this takes care of byte conversion too!
-        clientsocket.send(pickled_arr)
 
-        # receive the negated array from the client
-        msg = clientsocket.recv(1024)
-        negative_arr = pickle.loads(msg)
-        print(negative_arr)
-        
-    clientsocket.close()
+    arr = create_2d_array()
 
+    print(arr)
 
+    # send data to client
+    clientSocket.send(arr.tobytes())
+
+    # receive data from client
+    msg = clientSocket.recv(1024)
+
+    # decode data
+    result = numpy.frombuffer(msg, dtype=numpy.uint32).reshape((height, width))
+
+    print(result)
+
+    time.sleep(1)
+
+clientSocket.close()
